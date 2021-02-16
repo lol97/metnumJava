5dcfb6a39607b4b3a94373f2c8cb18f54d735b7d
lol97 <7879720@bukopin.co.id>
2021-02-16 22:44:49 +0700

    first init and integral numeric case trapezioda uniform data

---
+++ Trapezioda.java
@@ -0,0 +1,80 @@
+/**
+* JAVA CODE FOR TRAPEZIODA METTHOD INTEGRAL NUMERIC
+* UNIFORM DATA
+* BY WARTEGDUST 
+*/
+
+public class Trapezioda{
+	public float[] x;
+	public float[] y;
+	public float acceptableError = 0.0000001f;
+	public float distance;
+
+	public Trapezioda(float[] x, float[] y) {
+		this.x = x;
+		this.y = y;
+	}
+
+
+	public Trapezioda(float[] x, float[] y, float acceptableError) {
+		this.x = x;
+		this.y = y;
+		this.acceptableError = acceptableError;
+	}
+
+	public float estimateArea() throws Exception {
+		// count 
+		float area = 0.0f;
+		if (this.isDataValid()) {
+			float[] arrayX = this.x;
+			float[] arrayY = this.y;
+
+			float distance = arrayX[1] - arrayX[0];
+			float sumY = 0.0f;
+			for (float dataY : arrayY) {
+				sumY += dataY;
+			}
+
+			System.out.println("distance : " + distance);
+
+			area = (distance / 2) * (arrayY[0] + (2 * (sumY - arrayY[0] - arrayY[arrayY.length - 1] )) + arrayY[arrayY.length - 1] );
+
+			return area;
+
+		} else {
+			throw new Exception("selisih data pada koordinate x tidak seragam");
+		}
+	}
+
+	
+	public boolean isDataValid() throws Exception {
+		// function to check is x data valid
+					float initDistance = x[1] - x[0];
+			for (int idx = 1; idx < x.length - 1; idx++) {
+				float nextVar = x[idx + 1];
+				float befVar = x[idx];
+				float curDistance = (nextVar - befVar) - initDistance;
+				if (curDistance > acceptableError) {
+					return false;
+				}
+			}
+
+			return true;
+
+		
+	}
+
+	public static void main(String[] args) {
+		// FOR THEORY CHECK :: https://sufyan97.blogspot.com/2018/06/program-integral-numerik-metode.html
+
+		float[] dataX = {0.0f, 0.2f, 0.4f, 0.6f, 0.8f, 1.0f};
+		float[] dataY = {2.0f, 3.0f, 6.0f, 5.0f, 4.0f, 1.0f};
+
+		Trapezioda trpzd = new Trapezioda(dataX, dataY);
+		try {
+		System.out.println("area : " + trpzd.estimateArea());
+		} catch (Exception e) {
+			System.out.println("error : " + e.toString());
+		} 
+	}
+}
